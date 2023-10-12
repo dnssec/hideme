8446c0bd7043de80de1c08b8df2cb358e41d0e02
brokep <brokep@lavabit.com>
2023-10-12 15:04:01 -0500

    added more sites and site keys

--- name2.py
+++ name2.py
@@ -10,6 +10,9 @@ def do_search(site_key, first_name, last_name):
         'FastPeople': 'https://www.fastpeoplesearch.com/name/{first_name}-{last_name}',
         'Nuwber': 'https://nuwber.com/search?name={first_name}%20{last_name}',
         'XLEK': 'https://xlek.com/search_results.php?fname={first_name}&lname={last_name}&locations:all',
+        'Cubib': 'https://cubib.com/search_results.php?fname={first_name}&lname=&{last_name}&locations:all',
+        'FamilyTreeNow': 'https://www.familytreenow.com/search/genealogy/results?first&{first_name}&last=&{last_name}',
+        'Intelius': 'https://www.intelius.com/people-search/&{first_name}&-&{last_name}',
         # Add all other site templates here
     }
     
@@ -22,6 +25,9 @@ def do_all(first_name, last_name):
         'FastPeople',
         'Nuwber',
         'XLEK',
+        'Cubib',
+        'FamilyTreeNow'
+        'Intelius'
         # Add all other site keys here
     ]
     
