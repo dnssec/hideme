cc1d2ccc214e93b5868e078a9f655ae7b169d188
brokep <brokep@lavabit.com>
2023-10-26 03:43:47 -0500

    adding more sites  + keys and optoutlinks

---
+++ name4.1.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <windows.h>  // For opening URLs in Windows
+
+void doPopAll(const std::string& first_name, const std::string& last_name) {
+    // This function would populate all fields, but in a headless environment, it's not applicable.
+    std::cout << "Populating all fields with First Name: " << first_name << ", Last Name: " << last_name << std::endl;
+}
+
+void doSearch(const std::string& site_key, const std::string& first_name, const std::string& last_name) {
+    std::unordered_map<std::string, std::string> url_templates = {
+        {"411info", "https://411.info/people/?fn&" + first_name + "-" + last_name},
+        {"411", "https://www.411.com/person-search/" + first_name + "-" + last_name},
+        {"AllPeople", "https://allpeople.com/search?ss=" + first_name + "+" + last_name + "&ss-e=&ss-p=&ss-i=&where=&industry-auto=&where-auto="},
+        {"Addresses", "https://www.addresses.com/people/" + first_name + "+" + last_name},
+        {"Addresssearch", "https://www.addresssearch.com/people-search-full-name.php?fn=" + first_name + "&ln=" + last_name},
+        {"AdvancedBackgroundChecks", "https://www.advancedbackgroundchecks.com/names/" + first_name + "-" + last_name},
+        {"AdvancedPeopleSearch", "https://www.advanced-people-search.com/people/" + first_name + "-" + last_name},
+        {"Anywho", "https://www.anywho.com/people/" + first_name + "+" + last_name},
+        {"Archives", "https://www.archives.com/search/ancestor/results?FirstName=" + first_name + "&LastName=" + last_name + "&Country=United%20States&RecordSearchType=Unknown"},
+        {"Arrestfacts", "https://arrestfacts.com/ng/search?fname=" + first_name + "&lname=" + last_name + "&state=&city=&age=&type=&site="},
+        {"BackgroundAlerts", "https://www.backgroundalert.com/path/background/ln/1/results?searchbox=simple&SID=POLoxpD_5sLP7XYEyDA8aSqrOkZT8F9_&fn=" + first_name + "&ln=" + last_name},
+        {"BackgroundCheckRun", "https://backgroundcheck.run/ng/profile/search?fname=" + first_name + "&lname=" + last_name},
+        {"Blockshoppers", "https://blockshoppers.com/search/?q=" + first_name + "+" + last_name},
+        {"Calltruth", "https://www.calltruth.com/people/" + first_name + "-" + last_name},
+        {"Centeda", "https://centeda.com/profile/search?fname=" + first_name + "&lname=" + last_name + "&state=&city=&fage="},
+        {"CheckPeople", "https://checkpeople.com/landing/people/k1u2r3t4/searching?firstName=" + first_name + "&lastName=" + last_name + "&state=&city=&aid=11&sid=&tid=&hitid=&iv=&obcid="},
+        {"ClustrMaps", "https://clustrmaps.com/persons/" + first_name + "-" + last_name},
+        {"Classmates", "https://www.classmates.com/siteui/search/results?q=" + first_name + "%20" + last_name + "&searchType=all"},
+        {"Cubib", "https://cubib.com/search_results.php?fname=" + first_name + "&lname=&" + last_name + "&locations:all"},
+        {"CyberBackgroundChecks", "https://www.cyberbackgroundchecks.com/people/" + first_name + "&-&" + last_name},
+        {"Dataveria", "https://dataveria.com/profile/search?fname=" + first_name + "&lname=" + last_name + "&state=&city=&fage="},
+        {"FamilyTreeNow", "https://www.familytreenow.com/search/genealogy/results?first&" + first_name + "&last=&" + last_name},
+        {"FastPeople", "https://www.fastpeoplesearch.com/name/" + first_name + "-" + last_name},
+        {"FindPeopleSearch", "https://www.findpeoplesearch.com/" + first_name + "+" + last_name + "/1/16983092709439"},
+        {"Freepeopledirectory", "https://www.freepeopledirectory.com/name/" + first_name + "-" + last_name},
+        {"IDcrawl", "https://idcrawl.com/" + first_name + "-" + last_name},
+        {"Intelius", "https://www.intelius.com/people-search/&" + first_name + "&-&" + last_name},
+        {"Nuwber", "https://nuwber.com/search?name=" + first_name + "%20" + last_name},
+        {"OfficialUSA", "https://www.officialusa.com/names/" + first_name + "-" + last_name + "/"},
+        {"PeopleSearchNow", "https://www.peoplesearchnow.com/person/" + first_name + "-" + last_name},
+        {"PeoplebyName", "https://www.peoplebyname.com/people/" + last_name + "/" + first_name},
+        {"Radaris", "https://radaris.com/p/" + first_name + "&/&" + last_name},
+        {"RocketReach", "http://google.com/search?q=site:rocketreach.co+\"" + first_name + "&" + last_name + "&\""},
+        {"SearchPeopleFree", "https://www.searchpeoplefree.com/find/" + first_name + "&-&" + last_name},
+        {"Social-Searcher", "https://www.social-searcher.com/search-users/?q6=" + first_name + "&+&" + last_name},
+        {"Spokeo", "https://www.spokeo.com/" + first_name + "&-&" + last_name + "&?loaded=1"},
+        {"Spytox", "https://www.spytox.com/people/search?name=" + first_name + "&+&" + last_name},
+        {"ThatsThem", "https://thatsthem.com/name/" + first_name + "-" + last_name},
+        {"TruePeople", "https://www.truepeoplesearch.com/results?name=" + first_name + "%20" + last_name},
+        {"TruthFinder", "https://www.truthfinder.com/results/?firstName=" + first_name + "&&lastName=" + last_name + "&state=ALL"},
+        {"Webmii", "https://webmii.com/people?n=%22" + first_name + "%20" + last_name + "%22"},
+        {"WhitePages", "https://www.whitepages.com/name/" + first_name + "-" + last_name},
+        {"Yasni", "https://www.yasni.com/" + first_name + "+" + last_name + "/check+people"},
+        {"ZabaSearch", "https://www.zabasearch.com/people/" + first_name + "+" + last_name + "/"},
+        {"XLEK", "https://example.com/search/" + first_name + "+" + last_name} // Add your example URL here
+    };
+
+    auto it = url_templates.find(site_key);
+    if (it != url_templates.end()) {
+        std::string url = it->second;
+        ShellExecute(NULL, "open", url.c_str(), NULL, NULL, SW_SHOWNORMAL);
+    }
+}
+
+void doAll(const std::string& first_name, const std::string& last_name) {
+    std::string site_keys[] = {
+        "411info", "411", "Addresses", "Addresssearch", "AllPeople", "AdvancedBackgroundChecks", 
+        "AdvancedPeopleSearch", "Anywho", "Arrestfacts", "Archives", "BackgroundAlerts", 
+        "BackgroundCheckRun", "Blockshoppers", "Calltruth", "Centeda", "CheckPeople", "ClustrMaps", 
+        "Classmates", "Cubib", "CyberBackgroundChecks", "Dataveria", "FamilyTreeNow", "FastPeople", 
+        "FindPeopleSearch", "Freepeopledirectory", "IDcrawl", "Intelius", "Nuwber", "OfficialUSA", 
+        "PeopleSearchNow", "PeoplebyName", "Radaris", "RocketReach", "SearchPeopleFree", 
+        "Social-Searcher", "Spokeo", "Spytox", "ThatsThem", "TruePeople", "TruthFinder", 
+        "Webmii", "WhitePages", "Yasni", "ZabaSearch", "XLEK" // Add your example site key here
+    };
+
+    for (const std::string& site_key : site_keys) {
+        doSearch(site_key, first_name, last_name);
+    }
+}
+
+int main() {
+    // Example usage:
+
+    // Simulating the form submission for doPopAll
+    doPopAll("John", "Doe");
+
+    // Simulating individual form submissions
+    doSearch("TruePeople", "John", "Doe");
+    doSearch("FastPeople", "John", "Doe");
+
+    // Simulating 'Submit All' button
+    doAll("John", "Doe");
+
+    return 0;
+}
+
